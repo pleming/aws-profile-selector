a93eaca5f6808cdb92cae79b6d75ffcf2a509550
Hojin Choe <hojin.choe.dev@gmail.com>
2023-11-24 00:25:48 +0900

    [ISSUE-12] feat: feat: Implement new configuration creation

---
+++ public/js/common/const.js
@@ -0,0 +1,18 @@
+export default {
+    "ATTR": {
+        "PROFILE_NAME": "profileName"
+    },
+    "AWS_PROPERTY": {
+        "PROFILE": "profile",
+        "REGION": "region",
+        "AWS_ACCESS_KEY_ID": "aws_access_key_id",
+        "AWS_SECRET_ACCESS_KEY": "aws_secret_access_key",
+        "AWS_SESSION_TOKEN": "aws_session_token",
+        "SOURCE_PROFILE": "source_profile",
+        "MFA_ARN": "mfa_arn",
+        "OUTPUT": "output"
+    },
+    "LOCAL_STORAGE": {
+        "AWS_CONFIG": "awsConfig"
+    }
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,23 @@
 import configModifyModal from "./modal/config-modify-modal.js";
+import { globalData } from "./common/global-data.js";
+
+const initialize = () => {
+    globalData.initialize();
+};
 
 const registerEvent = () => {
+    $("#btnNewConfig").click(() => {
+        configModifyModal.initialize();
+        configModifyModal.show();
+    });
+
     $(".profile-button-container").on("click", ".btn-config-modify", () => {
         configModifyModal.show();
     });
 }
 
 $(() => {
+    initialize();
     registerEvent();
     configModifyModal.registerEvent();
 });
