0c3b451ce49d66f46773f2deb03b8239e30d1933
Hojin Choe <hojin.choe.dev@gmail.com>
2023-11-24 02:41:28 +0900

    [ISSUE-22] feat: Implement the OTP input function in the MFA profile

--- public/js/service/profile-service.js
+++ public/js/service/profile-service.js
@@ -1,16 +1,8 @@
 import Constants from "../common/const.js";
 
-const mfaContextMenuHTML =
-    `<div class="div-mfa-context-menu">
-        <li><a class="dropdown-item btn-input-mfa" href="#">MFA</a></li>
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-    </div>`;
-
 const profileButtonHTML =
     `<div class="btn-group btn-group-block btn-group-profile">
-        <button type="button" class="btn btn-outline-primary btn-split-main"></button>
+        <button type="button" class="btn btn-outline-primary btn-profile"></button>
         <button type="button" class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split btn-split-dropdown" data-bs-toggle="dropdown" aria-expanded="false">
             <span class="visually-hidden">Toggle Dropdown</span>
         </button>
@@ -20,14 +12,10 @@ const profileButtonHTML =
         </ul>
     </div>`;
 
-const appendProfile = (profileName, isMfaProfile) => {
+const appendProfile = (profileName) => {
     const profileButton = $($.parseHTML(profileButtonHTML));
 
-    if (isMfaProfile) {
-        profileButton.find(".dropdown-menu").prepend(mfaContextMenuHTML);
-    }
-
-    profileButton.find(".btn-split-main").text(profileName);
+    profileButton.find(".btn-profile").text(profileName);
     $(".profile-button-container").append(profileButton);
 };
 
