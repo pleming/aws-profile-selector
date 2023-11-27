6e824d8d4cf42e86fb9fb8deed4cc62d60f421ea
Hojin Choe <hojin.choe.dev@gmail.com>
2023-11-28 00:54:51 +0900

    [ISSUE-34] feat: Implement to execute confirm operation when pressing enter key in input field of modal

--- public/js/modal/profile-modify-modal.js
+++ public/js/modal/profile-modify-modal.js
@@ -43,58 +43,68 @@ const hide = () => {
     $("#profileModifyModal").modal("hide");
 };
 
+const saveProfile = async () => {
+    const response = await window.electronDialog.confirm("Save profile");
+
+    if (response === Constants.ELECTRON_DIALOG.CONFIRM.NO) {
+        return;
+    }
+
+    const newProfileName = $("input[name=inputProfileName]").val();
+
+    if (!newProfileName) {
+        window.electronDialog.alert("Profile name is empty");
+        return;
+    }
+
+    const awsProfile = profileService[Constants.LOCAL_STORAGE.AWS_PROFILE];
+
+    if (triggeredBy === Constants.TRIGGER.NEW_PROFILE && awsProfile.hasOwnProperty(newProfileName)) {
+        window.electronDialog.alert("Duplicated profile name");
+        return;
+    }
+
+    const newProfile = {
+        [Constants.AWS_PROPERTY.REGION]: $("input[name=inputRegion]").val(),
+        [Constants.AWS_PROPERTY.AWS_ACCESS_KEY_ID]: $("input[name=inputAccessKeyId]").val(),
+        [Constants.AWS_PROPERTY.AWS_SECRET_ACCESS_KEY]: $("input[name=inputAccessKey]").val()
+    };
+
+    const isMfaProfile = $("#switchActivateMfaArn").is(":checked");
+
+    if (isMfaProfile) {
+        newProfile[Constants.AWS_PROPERTY.MFA_ARN] = $("#inputMfaArn").val();
+    }
+
+    for (const key in newProfile) {
+        if (!newProfile[key]) {
+            window.electronDialog.alert(`${key} is empty`);
+            console.log();
+            return;
+        }
+    }
+
+    if (triggeredBy === Constants.TRIGGER.NEW_PROFILE) {
+        profileService.appendProfile(newProfileName, isMfaProfile);
+    } else if (triggeredBy === Constants.TRIGGER.MODIFY_PROFILE) {
+        delete awsProfile[profile.profileName];
+        profileButton.text(newProfileName);
+    }
+
+    awsProfile[newProfileName] = newProfile;
+    profileService.saveProfile(awsProfile);
+
+    hide();
+    initialize();
+};
+
 const registerEvent = () => {
     $("#switchActivateMfaArn").click((event) => {
         $("#inputMfaArn").prop("disabled", !$(event.target).is(":checked"));
     });
 
-    $("#btnSaveProfile").click(() => {
-        const newProfileName = $("input[name=inputProfileName]").val();
-
-        if (!newProfileName) {
-            window.electronDialog.alert("Profile name is empty");
-            return;
-        }
-
-        const awsProfile = profileService[Constants.LOCAL_STORAGE.AWS_PROFILE];
-
-        if (triggeredBy === Constants.TRIGGER.NEW_PROFILE && awsProfile.hasOwnProperty(newProfileName)) {
-            window.electronDialog.alert("Duplicated profile name");
-            return;
-        }
-
-        const newProfile = {
-            [Constants.AWS_PROPERTY.REGION]: $("input[name=inputRegion]").val(),
-            [Constants.AWS_PROPERTY.AWS_ACCESS_KEY_ID]: $("input[name=inputAccessKeyId]").val(),
-            [Constants.AWS_PROPERTY.AWS_SECRET_ACCESS_KEY]: $("input[name=inputAccessKey]").val()
-        };
-
-        const isMfaProfile = $("#switchActivateMfaArn").is(":checked");
-
-        if (isMfaProfile) {
-            newProfile[Constants.AWS_PROPERTY.MFA_ARN] = $("#inputMfaArn").val();
-        }
-
-        for (const key in newProfile) {
-            if (!newProfile[key]) {
-                window.electronDialog.alert(`${key} is empty`);
-                console.log();
-                return;
-            }
-        }
-
-        if (triggeredBy === Constants.TRIGGER.NEW_PROFILE) {
-            profileService.appendProfile(newProfileName, isMfaProfile);
-        } else if (triggeredBy === Constants.TRIGGER.MODIFY_PROFILE) {
-            delete awsProfile[profile.profileName];
-            profileButton.text(newProfileName);
-        }
-
-        awsProfile[newProfileName] = newProfile;
-        profileService.saveProfile(awsProfile);
-
-        hide();
-        initialize();
+    $("#btnSaveProfile").click(async () => {
+        await saveProfile();
     });
 
     $("#btnProfileModifyModalCancel").click(async () => {
@@ -104,6 +114,12 @@ const registerEvent = () => {
     $("#btnProfileModifyModalClose").click(async () => {
         await closeProfileModifyModal();
         hide();
+    });
+
+    $(".input-profile-modify").on("keypress", async (event) => {
+        if (event.keyCode === 13) {
+            await saveProfile();
+        }
     });
 
     $("#profileModifyModal").on("shown.bs.modal", () => {
